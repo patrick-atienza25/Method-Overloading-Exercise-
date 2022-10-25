2248eacef22f7451afbfadea9fabf329c02b5aa8
patrick-atienza25 <patienza25@gmail.com>
2022-10-25 09:36:20 -0700

    finished exercise

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,15 +6,18 @@ namespace MethodOverloading
     {
         static void Main(string[] args)
         {
-            Add(6.57m, 5.32m, true);
+           Console.WriteLine(Add(1.50m, 1.50m, true));
+           Console.WriteLine(Add(2,4, true));
+           Console.WriteLine(Add(3.75m, 5, true));
+           Console.WriteLine(Add(2.02m, 2.12m, true));
         }
-        static int Add(int num1, int num2)
+        public static int Add(int num1, int num2)
         {
-            return num1 + num2;
+             return num1 + num2;
         }
         public static decimal Add(decimal num1, decimal num2)
         {
-            return num1 + num2;
+             return num1 + num2;
         }
         public static string Add(int num1, int num2, bool isCheck)
         {
@@ -29,7 +32,27 @@ namespace MethodOverloading
             }
             else if (isCheck == true && sum < 1)
             {
-                return $"{sum} dollars");
+                return $"{sum} dollars";
+            }
+            else
+            {
+                return sum.ToString();
+            }
+        }
+        public static string Add(decimal num1, decimal num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars";
             }
             else
             {
