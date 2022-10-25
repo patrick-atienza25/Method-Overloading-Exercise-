ed2951d21bf8842ae920ec8a3d2ded4eb77fbde9
patrick atienza <patienza25@gmail.com>
2022-10-24 21:03:17 -0700

    finished two exercises

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodOverloading
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(6.57m, 5.32m, true);
+        }
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int num1, int num2, bool isCheck)
+        {
+            var sum = num1 + num2;
+            if (isCheck == true && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+            else if (isCheck == true && sum == 1)
+            {
+                return $"{sum} dollar";
+            }
+            else if (isCheck == true && sum < 1)
+            {
+                return $"{sum} dollars");
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
